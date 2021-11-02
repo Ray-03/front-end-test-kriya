b04cc39fede3d3645ebe4ccf2367a4b7d108f87a
ray-03 <rinaldy.richard.03@gmail.com>
2021-11-02 12:12:07 +0700

    Create product list view (page 1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_end_test_kriya/view/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const ProductListView(),
     );
   }
 }
---
+++ lib/view/product_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductListView extends StatelessWidget {
+  const ProductListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(),
+    );
+  }
+}

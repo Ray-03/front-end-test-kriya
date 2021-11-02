48f409963f393a9d590deca4a6ce1755c0259262
ray-03 <rinaldy.richard.03@gmail.com>
2021-11-02 12:15:38 +0700

    Add appbar to count total product bought

--- lib/view/product_list.dart
+++ lib/view/product_list.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-class ProductListView extends StatelessWidget {
+class ProductListView extends StatefulWidget {
   const ProductListView({Key? key}) : super(key: key);
 
   @override
+  State<ProductListView> createState() => _ProductListViewState();
+}
+
+class _ProductListViewState extends State<ProductListView> {
+  int totalProductQty = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Total Product qty = $totalProductQty'),
+        ),
+      ),
     );
   }
 }

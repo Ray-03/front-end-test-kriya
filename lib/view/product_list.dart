a94ed5b7188bc08b6dafed4f26e006a6aed7fe1d
ray-03 <rinaldy.richard.03@gmail.com>
2021-11-02 19:02:57 +0700

    Get product from API and show list of product

--- lib/view/product_list.dart
+++ lib/view/product_list.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:front_end_test_kriya/const.dart';
+import 'package:front_end_test_kriya/model/product.dart';
+import 'package:pagination_view/pagination_view.dart';
+import 'package:http/http.dart' as Http;
 
 class ProductListView extends StatefulWidget {
   const ProductListView({Key? key}) : super(key: key);
@@ -8,7 +14,24 @@ class ProductListView extends StatefulWidget {
 }
 
 class _ProductListViewState extends State<ProductListView> {
+  int page = 1;
   int totalProductQty = 0;
+
+  Future<List<Product>> pageFetch(int currListSize) async {
+    Http.Response json = await Http.get(
+      Uri.parse('$baseUrl?$paginationArgs=$page'),
+    );
+    List<dynamic> jsonData = jsonDecode(json.body);
+    final List<Product> nextProductsList = List.generate(
+      jsonData.length,
+      (int index) => Product.fromJson(
+        jsonData[index],
+        currListSize + index,
+      ),
+    );
+    page = (currListSize / 10).round();
+    return jsonData.isEmpty ? [] : nextProductsList;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +39,21 @@ class _ProductListViewState extends State<ProductListView> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Total Product qty = $totalProductQty'),
+        ),
+        body: PaginationView(
+          pullToRefresh: true,
+          itemBuilder: (BuildContext context, Product product, int index) {
+            return ListTile(
+              title: Text(product.name),
+            );
+          },
+          pageFetch: pageFetch,
+          onError: (dynamic error) => const Center(
+            child: Text('Some error occured'),
+          ),
+          onEmpty: const Center(
+            child: Text('Sorry! This is empty'),
+          ),
         ),
       ),
     );

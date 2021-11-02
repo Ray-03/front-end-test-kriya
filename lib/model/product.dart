b9a0036d6ce9ebf12bc68027d7995f1dafb47e54
ray-03 <rinaldy.richard.03@gmail.com>
2021-11-02 14:27:39 +0700

    modify product model

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,11 +1,14 @@
 class Product {
   Product({
     required this.name,
+    this.productId,
   });
 
   String name = '-';
+  String? productId;
 
-  Product.fromJson(dynamic json) {
+  Product.fromJson(dynamic json, int index) {
     name = json['title'];
+    productId = 'U-' + index.toString().padLeft(4, '0');
   }
 }

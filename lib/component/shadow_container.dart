0a105f15333972c9fd6412262ca544551b2c528f
ray-03 <rinaldy.richard.03@gmail.com>
2021-11-03 15:50:43 +0700

    Create container with shadow component

---
+++ lib/component/shadow_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ShadowContainer extends StatelessWidget {
+  const ShadowContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}

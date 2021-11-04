88a175dbf8998c77af8739df00bbda7782410ffd
ray-03 <rinaldy.richard.03@gmail.com>
2021-11-04 12:54:12 +0700

    Add route

---
+++ lib/route.dart
@@ -0,0 +1,7 @@
+import 'package:front_end_test_kriya/view/checkout.dart';
+import 'package:front_end_test_kriya/view/product_list.dart';
+
+var route = {
+  ProductListView.id: (context) => const ProductListView(),
+  CheckoutView.id: (context) => const CheckoutView(),
+};
